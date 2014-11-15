e79ef5a9eb5f3e061bee2a87d29d59559c3125a1
Paulo Chang <pigenio@gmail.com>
2014-11-15 13:03:52 -0600

    Commit Inicial

---
+++ UVGProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UVGProject.MvcApplication" Language="C#" %>
---
+++ UVGProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UVGProject.Startup))]
+namespace UVGProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
