532c3250b1040f39869ade44d7e75d204871762c
dennissescobar <dennissescobar@gmail.com>
2014-11-18 15:21:17 -0600

    agregar cursos y archivos

---
+++ UVGProject/Controllers/CursoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UVGProject.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Crusos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UVGProject/Controllers/CursosArchivoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UVGProject.Controllers
+{
+    public class CursosArchivoController : Controller
+    {
+        // GET: CursosArchivo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
